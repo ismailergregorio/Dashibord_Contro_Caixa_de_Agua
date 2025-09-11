0358901e6614d62509238862b051ddfc6c521de9
ismailergregorio <ismailergregorio6@gmail.com>
2025-09-10 21:43:52 -0300

    Iniciando Criação Caixa de controle para a bomba

--- src/d-controle/caixa-de-controle-bomba/main-caixa-bomba.jsx
+++ src/d-controle/caixa-de-controle-bomba/main-caixa-bomba.jsx
@@ -6,8 +6,11 @@ export default function ControleBomda() {
     <div className="controleBomba">
       <div className="controle">
         <div className="controleBtn">
-          <h1>Ligar/Desligar:</h1>
-          <input type="checkbox" />
+          <input type="checkbox" id="btnMotor" />
+          <div className="Slider" >
+            <div className="circulo"></div>
+          </div>
+
         </div>
         <div className="statusMotor">
           <h1>Status:</h1>
