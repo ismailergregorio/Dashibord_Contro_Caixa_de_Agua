013fcd4f146fc58c9d423c975e3038c56ea07ce6
ismailergregorio <ismailergregorio6@gmail.com>
2026-01-10 20:22:36 -0300

    .

--- src/d-controle/caixa-de-controle-sensores/main-caixa-sensores.jsx
+++ src/d-controle/caixa-de-controle-sensores/main-caixa-sensores.jsx
@@ -4,9 +4,9 @@ import StatusSensor from "../../components/controle-sensor/status-sensor"
 export default function CaixaSensores({sensor1,sensor2,sensor3}){
  return(
   <div className="sensores">
-   <StatusSensor texto="1" stado={sensor1}/>
+   <StatusSensor texto="1" stado={sensor3}/>
    <StatusSensor texto="2" stado={sensor2}/>
-   <StatusSensor texto="3" stado={sensor3}/>
+   <StatusSensor texto="3" stado={sensor1}/>
   </div>
  )
 }
