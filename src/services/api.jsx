7452f3eaba3eff71012fd4379128522124313a54
ismailergregorio <ismailergregorio6@gmail.com>
2026-01-22 00:30:11 -0300

    .

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -4,6 +4,12 @@ const api = axios.create({
   baseURL: "http://192.168.100.5:8081"
 });
 
+export default api;
+
+const refreshApi = axios.create({
+  baseURL: "http://192.168.100.5:8081"
+});
+
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("accessToken");
 
@@ -18,40 +24,36 @@ api.interceptors.response.use(
   res => res,
   async error => {
     const originalRequest = error.config;
-    const status = error.response?.status;
 
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      try {
+      // try {
         const refresh = localStorage.getItem("refreshToken");
 
-        const { data } = await api.post("/refresh", {
-          refreshToken: refresh
+        const resposta = await refreshApi.post("/auth/refresh", {
+          refresh:refresh
         });
 
-        localStorage.setItem("accessToken", data.accessToken);
-        localStorage.setItem("refreshToken", data.refreshToken);
+        const { toker, refreshToker } = resposta.data;
 
-        originalRequest.headers.Authorization =
-          `Bearer ${data.accessToken}`;
+        localStorage.setItem("accessToken", toker);
+        localStorage.setItem("refreshToken", refreshToker);
+
+        console.log("novo"+toker)
+        console.log("novo"+refreshToker)
+        originalRequest.headers.Authorization = `Bearer ${toker}`;
 
         return api(originalRequest);
 
-      } catch (err) {
-        // Falhou o refresh → logout forçado
-        localStorage.clear();
-        window.location.href = "/";
-      }
+      // } catch (err) {
+      //   console.error("Erro no refresh", err);
+      //   localStorage.clear();
+      //   window.location.href = "/";
+      //   return Promise.reject(err);
+      // }
     }
-
-    // if (status === 403) {
-    //   localStorage.clear();
-    //   window.location.href = "/";
-    // }
 
     return Promise.reject(error);
   }
 );
-
-export default api;
