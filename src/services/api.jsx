4823b9af067cefad900cd23bd2e14e4a3a95d4eb
ismailergregorio <ismailergregorio6@gmail.com>
2026-01-22 17:41:58 -0300

    .

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,16 +1,17 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: "http://192.168.100.5:8081"
+  baseURL: "http://localhost:8081",
 });
 
 export default api;
 
 const refreshApi = axios.create({
-  baseURL: "http://192.168.100.5:8081"
+  baseURL: "http://localhost:8081",
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
 
   if (token) {
@@ -21,18 +22,29 @@ api.interceptors.request.use(config => {
 });
 
 api.interceptors.response.use(
-  res => res,
-  async error => {
-    const originalRequest = error.config;
+  (res) => res,
+  async (error) => {
+    // ❌ erro de conexão (backend fora)
+    if (!error.response) {
+      console.error("Backend indisponível");
 
+      // opcional: aviso ao usuário
+      toast.error("Servidor fora do ar");
+
+      return Promise.reject({
+        message: "Servidor indisponível",
+      });
+    }
+
+    const originalRequest = error.config;
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      // try {
-        const refresh = localStorage.getItem("refreshToken");
+      try {
+        const refrash = localStorage.getItem("refreshToken");
 
         const resposta = await refreshApi.post("/auth/refresh", {
-          refresh:refresh
+          refrash: refrash,
         });
 
         const { toker, refreshToker } = resposta.data;
@@ -40,20 +52,17 @@ api.interceptors.response.use(
         localStorage.setItem("accessToken", toker);
         localStorage.setItem("refreshToken", refreshToker);
 
-        console.log("novo"+toker)
-        console.log("novo"+refreshToker)
         originalRequest.headers.Authorization = `Bearer ${toker}`;
 
         return api(originalRequest);
-
-      // } catch (err) {
-      //   console.error("Erro no refresh", err);
-      //   localStorage.clear();
-      //   window.location.href = "/";
-      //   return Promise.reject(err);
-      // }
+      } catch (err) {
+        console.error("Erro no refresh", err);
+        localStorage.clear();
+        window.location.href = "/";
+        return Promise.reject(err);
+      }
     }
 
     return Promise.reject(error);
-  }
+  },
 );
